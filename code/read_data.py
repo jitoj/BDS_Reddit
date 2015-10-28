cb509362862d6d5bff5e1ae86e7e655f11544da9
jitoj <jitesh.jagadish@gmail.com>
2015-10-27 21:34:40 -0400

    Added graceful exit for interuppt signal

--- code/read_data.py
+++ code/read_data.py
@@ -1,14 +1,26 @@
 import os
 import sys
 import json
+import signal
 from collections import Counter,defaultdict
 
 redditcount=defaultdict(Counter)
 line_count=1
+
+def signal_handler(signal, frame):
+	if(raw_input("\nQuit [y/n]?").lower()=='y'):
+		close_file(infile)
+		write_output(outfile)
+		close_file(outfile)
+		quit()
+	else:
+		return
+
+
 def process_line(data):
 	global redditcount,line_count
 	try:
-		print "Line:",line_count," ",data["subreddit"]," ",data["author"]
+		print "Reading line:",line_count," ",data["subreddit"]," ",data["author"]
 		redditcount[data["subreddit"]][data["author"]]+=1
 		line_count+=1
 	except:
@@ -30,23 +42,36 @@ def process_file(infile,outfile):
 	except:
 		return
 
+def write_output(outfile):
+	global redditcount
+	try:
+		for subreddit,authors in redditcount.iteritems():		
+			outfile.write(subreddit+','+str(len(authors)))
+			print "Writing subreddit:",subreddit
+			for author,count in authors.iteritems():
+				outfile.write(','+author+','+str(count))
+			outfile.write('\n')
+	except:
+		return	
+
+def close_file(closefile):
+	closefile.close()
+	return
+
+
 if __name__ == "__main__":
+	signal.signal(signal.SIGINT, signal_handler)
+	signal.signal(signal.SIGTSTP, signal_handler)
 	global line
+	#infile = open('data/RC_2015-05','r')
+	#outfile = open('data/redditcounts.csv','w+')	
 	infile = open('../data/li.txt','r')
 	outfile = open('../data/redditcounts.csv','w+')
 	try:
 		process_file(infile,outfile)
+		close_file(infile)
 	except:
 		pass
-	try:
-		for subreddit,authors in redditcount.iteritems():		
-			outfile.write(subreddit+','+str(len(authors)))
-			for author,count in authors.iteritems():
-				print subreddit
-				outfile.write(','+author+','+str(count))
-			outfile.write('\n')
-	except:
-		pass
-	infile.close()
-	outfile.close()
+	write_output(outfile)
+	close_file(outfile)
 	quit()
